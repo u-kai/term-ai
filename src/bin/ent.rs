ac5b8b22a0af6bba4f29b49b94d2ffaf4e1e92de
u-kai <76635578+u-kai@users.noreply.github.com>
2023-05-27 10:20:17 +0900

    fix ent command
    found good way to give instructions,so reised them

--- src/bin/ent.rs
+++ src/bin/ent.rs
@@ -3,7 +3,8 @@ use term_ai::gpt::GptClient;
 fn main() {
     println!("Welcome to GPT3 English Teacher REPL");
     let mut gpt = GptClient::from_env().unwrap();
-    let message = String::from("今後全ての私の発言を英語に翻訳して,日本語で解説してください.");
+    let message =
+        String::from("あなたは英語の先生です.今後全ての私の発言を英語に翻訳して,翻訳した結果をなぜその英訳になったのか,日本語で解説してください.");
     gpt.repl_gpt3_5_with_first_command(message.as_str())
         .unwrap();
 }
